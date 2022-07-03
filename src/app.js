d7204cab0826ab159c2d86f7c0431524f132df02
kposka <karolisposka96@gmail.com>
2022-07-03 22:06:42 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { port, nodeMailerConfig } = require("./config");
+const mailer = require("nodemailer");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+console.log(nodeMailerConfig);
+
+const sendEmail = async (email, text) => {
+  let transporter = await mailer.createTransport({
+    host: nodeMailerConfig.host,
+    port: nodeMailerConfig.port,
+    secure: true,
+    auth: {
+      user: nodeMailerConfig.user,
+      pass: nodeMailerConfig.pass,
+    },
+  });
+
+  let config = await transporter.sendMail({
+    from: `"Pill" <${nodeMailerConfig.user}>`,
+    to: email,
+    subject: "Notification",
+    text: text,
+  });
+  console.log(config);
+};
+
+app.post("/", async (req, res) => {
+  try {
+    if (req.body.email && req.body.text) {
+      await sendEmail(req.body.email, req.body.text);
+      return res.send({ msg: "email is send" });
+    }
+    return res
+      .status(500)
+      .send({ err: "something wrong with the server. Please try again later" });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ err: "something wrong with the server. Please try again later" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port} port`);
+});
---
+++ src/config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+  port: process.env.PORT,
+  nodeMailerConfig: {
+    host: process.env.MAILER_HOST,
+    port: process.env.MAILER_PORT,
+    pass: process.env.MAILER_PASSWORD,
+    user: process.env.MAILER_USER,
+  },
+};
